0480ea2f375a100d6b747fe638783b15924b3d34
Bakresh <bakresh@gmail.com>
2022-10-04 13:56:21 -0400

    Add the playRound function

--- javascript.js
+++ javascript.js
@@ -12,3 +12,33 @@ function getComputerChoice() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats rock.";
+        } else if (computerSelection === "scissors") {
+            return "You win! Rock beats scissors";
+        } else {
+            return "Tie! Rocks make friends!";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats rock.";
+        } else if (computerSelection === "scissors") {
+            return "You lose! Scissors beat paper.";
+        } else {
+            return "Tie! Papers make friends!";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats scissors.";
+        } else if (computerSelection === "paper") {
+            return "You win! Scissors beat paper.";
+        } else {
+            return "Tie! Scissors make friends!";
+        }
+    } else {
+        return "What kind of move is that?!? Invalid! INVALID!!! Game's over, you fool!";
+    }
+}
+
