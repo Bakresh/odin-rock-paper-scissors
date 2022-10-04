3be98f7591d74ec8f15b0e6a1448f190cba79323
Bakresh <bakresh@gmail.com>
2022-10-04 14:27:40 -0400

    Add game function and modify playRound function

--- javascript.js
+++ javascript.js
@@ -15,30 +15,71 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            return "You lose! Paper beats rock.";
+            return result = "You lose! Paper beats rock.";
+            computerScore++;
         } else if (computerSelection === "scissors") {
-            return "You win! Rock beats scissors";
+            return result = "You win! Rock beats scissors";
+            playerScore++;
         } else {
-            return "Tie! Rocks make friends!";
+            return result = "Tie! Rocks make friends!";
+            playerScore++;
+            computerScore++;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            return "You win! Paper beats rock.";
+            return result = "You win! Paper beats rock.";
+            playerScore++;
         } else if (computerSelection === "scissors") {
-            return "You lose! Scissors beat paper.";
+            return result = "You lose! Scissors beat paper.";
+            computerScore++;
         } else {
-            return "Tie! Papers make friends!";
+            return result = "Tie! Papers make friends!";
+            playerScore++;
+            computerScore++;
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            return "You lose! Rock beats scissors.";
+            return result = "You lose! Rock beats scissors.";
+            computerScore++;
         } else if (computerSelection === "paper") {
-            return "You win! Scissors beat paper.";
+            return result = "You win! Scissors beat paper.";
+            playerScore++;
         } else {
-            return "Tie! Scissors make friends!";
+            return result = "Tie! Scissors make friends!";
+            playerScore++;
+            computerScore++;
         }
     } else {
-        return "What kind of move is that?!? Invalid! INVALID!!! Game's over, you fool!";
+        return result = "What kind of move is that?!? Invalid! INVALID!!! Game's over, you fool!";
+        playerScore = 0;
+        computerScore = 1000000000;
     }
 }
 
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Make your move.").toLowerCase();
+        let computerSelection = getComputerChoice();
+        if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+            playRound(playerSelection, computerSelection);
+            console.log(result);
+            console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}.`);
+        } else {
+            playRound(playerSelection, computerSelection);
+            console.log(result);
+            console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}.`);
+            break;
+        }
+    }
+    if (playerScore < computerScore) {
+        console.log("Dang! Computer recks you!");
+    } else if (playerScore > computerScore) {
+        console.log("Whoa! You reck computer!");
+    } else {
+        console.log("Draw!");
+    }
+}
+
+game();
